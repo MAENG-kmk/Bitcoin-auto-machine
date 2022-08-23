9fe125cb88868ba5ffe360538ee0ec70836c249f
Kim mingi <kmk2737@snu.ac.kr>
2022-08-23 16:56:00 +0900

    backtest.

--- backtesting.py
+++ backtesting.py
@@ -19,7 +19,7 @@ btc = binance.fetch_ohlcv(
         symbol=symbol,
         timeframe='4h', 
         since=None, 
-        limit=6 * 30 * 3
+        limit=6 * 30 * 6
     )
 df = pd.DataFrame(data=btc, columns=['datetime', 'open', 'high', 'low', 'close', 'volume'])
 df['datetime'] = pd.to_datetime(df['datetime'], unit='ms')
@@ -56,23 +56,23 @@ def rsi(df):
 def candle(df):
 
     if df.iloc[-1]['high'] - max(df.iloc[-1]['open'], df.iloc[-1]['close']) > df.iloc[-1]['body']:
-        if df.iloc[-1]['high'] - max(df.iloc[-1]['open'], df.iloc[-1]['close']) > 200:
+        if df.iloc[-1]['high'] - max(df.iloc[-1]['open'], df.iloc[-1]['close']) > 0:
             return "meteor" # 유성형
     
     if min(df.iloc[-1]['open'], df.iloc[-1]['close']) - df.iloc[-1]['low'] > df.iloc[-1]['body']:
-        if min(df.iloc[-1]['open'], df.iloc[-1]['close']) - df.iloc[-1]['low'] > 200:
+        if min(df.iloc[-1]['open'], df.iloc[-1]['close']) - df.iloc[-1]['low'] > 0:
             return "hammer" # 망치형
 
 # 거래량 비교
 def volume(df):
     volume1 = df.iloc[-1]['volume']
     volume2 = df.iloc[-2]['volume']
-    if volume1 > volume2 * 1.5:
+    if volume1 > volume2 * 0.4:
         return "go"
     else:
         return "stop"
 
-#백테스팅 코드
+#백테스팅 코드 1 -실패
 class candle_volume_rsi:
     def __init__(self, data, balance):
         self.data = data        # 4h 차트 데이터
@@ -117,7 +117,7 @@ class candle_volume_rsi:
                     self.position = None
                     self.price, self.win_price, self.lose_price = 0, 0, 0
                     self.win += 1
-                elif self.data.iloc[i]['high'] >= self.win_price and self.data.iloc[i]['low'] <= self.lose_price:
+                elif self.data.iloc[i]['high'] >= self.lose_price and self.data.iloc[i]['low'] <= self.lose_price:
                     self.ror *= (0.95 - self.fee)
                     self.position = None
                     self.price, self.win_price, self.lose_price = 0, 0, 0
@@ -128,7 +128,7 @@ class candle_volume_rsi:
                     self.position = None
                     self.price, self.win_price, self.lose_price = 0, 0, 0
                     self.win += 1
-                elif self.data.iloc[i]['high'] >= self.win_price and self.data.iloc[i]['low'] <= self.lose_price:
+                elif self.data.iloc[i]['high'] >= self.lose_price and self.data.iloc[i]['low'] <= self.lose_price:
                     self.ror *= (0.95 - self.fee)
                     self.position = None
                     self.price, self.win_price, self.lose_price = 0, 0, 0
@@ -138,7 +138,86 @@ class candle_volume_rsi:
         print("승리횟수: ", self.win)
         print("패배횟수: ", self.lose)
             
-            
-backtest = candle_volume_rsi(df, 10000)
+
+#백테스팅 코드 2
+class candle_volume_rsi_1candle:
+    def __init__(self, data, balance):
+        self.data = data        # 4h 차트 데이터
+        self.balance = balance  # 잔고
+        self.fee = 0.008        # 매수 매도 총 수수료
+        #포지션
+        self.position = None
+        # 수익률
+        self.ror= 1
+        # 승리 횟수
+        self.win = 0
+        # 패배 횟수
+        self.lose = 0
+        #체결가
+        self.price = 0
+        #이익 판매가
+        self.win_price = 0
+        #손해 판매가
+        self.lose_price = 0
+    
+    def excute(self):
+        for i in range(20, len(self.data)):
+            df = self.data[i - 16:i + 1]
+            if self.position == None:
+                if candle(df) == "meteor":
+                    if volume(df) == "go":
+                        if rsi(df) > 45:
+                            self.position = "short"
+                            self.price = self.data.iloc[i]['close']
+                            self.lose_price = self.price * 1.01
+                elif candle(df) == "hammer":
+                    if volume(df) == "go":
+                        if rsi(df) < 45:
+                            self.position = "long"
+                            self.price = self.data.iloc[i]['close']
+                            self.lose_price = self.price * 0.99
+            elif self.position == "short":
+                sell_price = self.data.iloc[i]['close']
+                percent = sell_price / self.price
+                if sell_price > self.lose_price:
+                    self.lose += 1
+                    self.ror *= 0.9 - self.fee
+                    self.position = None
+                    self.price, sell_price, self.lose_price = 0, 0, 0
+                elif sell_price < self.price:
+                    self.ror *= 1 + (1 - percent) * 10 - self.fee
+                    self.win += 1
+                    self.position = None
+                    self.price, sell_price, self.lose_price = 0, 0, 0
+                else:
+                    self.ror *= 1 - (percent - 1) * 10 - self.fee
+                    self.lose += 1
+                    self.position = None
+                    self.price, sell_price, self.lose_price = 0, 0, 0                    
+                    
+                    
+            else:
+                sell_price = self.data.iloc[i]['close']
+                percent = sell_price / self.price
+                if sell_price < self.lose_price:
+                    self.lose += 1
+                    self.ror *= 0.9 - self.fee
+                    self.position = None
+                    self.price, sell_price, self.lose_price = 0, 0, 0
+                elif sell_price > self.price:
+                    self.ror *= 1 + (percent - 1) * 10 - self.fee
+                    self.win += 1
+                    self.position = None
+                    self.price, sell_price, self.lose_price = 0, 0, 0
+                else:
+                    self.ror *= 1 + (percent - 1) * 10 - self.fee
+                    self.lose += 1
+                    self.position = None
+                    self.price, sell_price, self.lose_price = 0, 0, 0   
+    def result(self):
+        print("수익률: ", self.ror)
+        print("승리횟수: ", self.win)
+        print("패배횟수: ", self.lose)
+backtest = candle_volume_rsi_1candle(df, 10000)
 backtest.excute()
 backtest.result()
---
+++ tools_cvr.py
@@ -0,0 +1,46 @@
+import pandas as pd
+
+# 캔들 모양
+def candle(df):
+
+    if df.iloc[-1]['high'] - max(df.iloc[-1]['open'], df.iloc[-1]['close']) > df.iloc[-1]['body']:
+        if df.iloc[-1]['high'] - max(df.iloc[-1]['open'], df.iloc[-1]['close']) > 200:
+            return "meteor" # 유성형
+    
+    if min(df.iloc[-1]['open'], df.iloc[-1]['close']) - df.iloc[-1]['low'] > df.iloc[-1]['body']:
+        if min(df.iloc[-1]['open'], df.iloc[-1]['close']) - df.iloc[-1]['low'] > 200:
+            return "hammer" # 망치형
+        
+# rsi 지표
+def rsi(df): 
+    au = 0
+    u = 0
+    ad = 0
+    d = 0
+    # cur = cur_price - df.iloc[-1]['open']
+    for i in df.iloc[-1:-15:-1]['size']:
+        if i >= 0:
+            au += i
+            u += 1
+        else:
+            ad += -i
+            d += 1
+    au /= u
+    ad /= d
+    # if cur >= 0:
+    #     au += cur / 14
+    # else:
+    #     ad += cur / 14 * (-1)
+    rs = au / ad
+    rsi = rs / ( 1 + rs) * 100
+    
+    return rsi
+
+# 거래량 비교
+def volume(df):
+    volume1 = df.iloc[-1]['volume']
+    volume2 = df.iloc[-2]['volume']
+    if volume1 > volume2 * 1.5:
+        return "go"
+    else:
+        return "stop"
